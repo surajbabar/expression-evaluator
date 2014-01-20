ac35cdb51dc683c6d5728d825909faff32b15839
suraj <surajbab@thoughtworks.com>
2014-01-20 17:44:10 +0530

    simple test done

--- tests/surajbab/evaluator/Exp_evaluatorTest.java
+++ tests/surajbab/evaluator/Exp_evaluatorTest.java
@@ -2,10 +2,12 @@ package surajbab.evaluator;
 
 import org.junit.Test;
 
+import static junit.framework.Assert.assertEquals;
+
 public class Exp_evaluatorTest {
     @Test
     public void testEvaluate() throws Exception {
         Exp_evaluator expEval = new Exp_evaluator();
-        assert(1==expEval.evaluate("0+1"));
+        assertEquals(1,expEval.evaluate("0+1"));
     }
 }
