0a0feecbe73bdfdab87e2c84f5845edb56ff64ba
suraj <surajbab@thoughtworks.com>
2014-01-21 19:07:47 +0530

    working for 5 operators with two positive integers

--- src/surajbab/evaluator/Exp_evaluator.java
+++ src/surajbab/evaluator/Exp_evaluator.java
@@ -2,7 +2,23 @@ package surajbab.evaluator;
 
 public class Exp_evaluator {
     public int evaluate(String Exp){
-
-        return  1;
+        int result=0;
+       Exp=Exp.replaceAll(" +", "");
+        char Operators[] ={'+','-','*','/','^'};
+        String numbers =Exp;
+        for (char operator : Operators)
+          numbers =numbers.replace(operator,' ');
+        String Numbers[] = numbers.split(" ");
+        if(Exp.charAt(1)=='+')
+            result =Integer.parseInt(Numbers[0])+Integer.parseInt(Numbers[1]);
+        if(Exp.charAt(1)=='-')
+            result =Integer.parseInt(Numbers[0])-Integer.parseInt(Numbers[1]);
+        if(Exp.charAt(1)=='*')
+            result =Integer.parseInt(Numbers[0])*Integer.parseInt(Numbers[1]);
+        if(Exp.charAt(1)=='/')
+            result =Integer.parseInt(Numbers[0])/Integer.parseInt(Numbers[1]);
+        if(Exp.charAt(1)=='^')
+            result =Integer.parseInt(Numbers[0])^Integer.parseInt(Numbers[1]);
+        return result;
     }
 }
--- tests/surajbab/evaluator/Exp_evaluatorTest.java
+++ tests/surajbab/evaluator/Exp_evaluatorTest.java
@@ -6,8 +6,28 @@ import static junit.framework.Assert.assertEquals;
 
 public class Exp_evaluatorTest {
     @Test
-    public void testEvaluate() throws Exception {
+    public void testEvaluateForTwoPositiveIntegersAndPlus() throws Exception {
         Exp_evaluator expEval = new Exp_evaluator();
-        assertEquals(1,expEval.evaluate("0+1"));
+        assertEquals(1,expEval.evaluate("0   +1"));
+    }
+    @Test
+    public void testEvaluateForTwoPositiveIntegersAndMinus() throws Exception {
+        Exp_evaluator expEval = new Exp_evaluator();
+        assertEquals(0,expEval.evaluate("1   -1"));
+    }
+    @Test
+    public void testEvaluateForTwoPositiveIntegersAndDivide() throws Exception {
+        Exp_evaluator expEval = new Exp_evaluator();
+        assertEquals(1,expEval.evaluate("1   /1"));
+    }
+    @Test
+    public void testEvaluateForTwoPositiveIntegersAndMultiplication() throws Exception {
+        Exp_evaluator expEval = new Exp_evaluator();
+        assertEquals(1,expEval.evaluate("1   *1"));
+    }
+    @Test
+    public void testEvaluateForTwoPositiveIntegersAndPower() throws Exception {
+        Exp_evaluator expEval = new Exp_evaluator();
+        assertEquals(0,expEval.evaluate("1   ^1"));
     }
 }
