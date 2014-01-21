ae984153bc5538c05d0299bb4087f7946a55d23c
suraj <surajbab@thoughtworks.com>
2014-01-21 20:05:55 +0530

    extracted getNumbers method from evaluate method

--- src/surajbab/evaluator/Exp_evaluator.java
+++ src/surajbab/evaluator/Exp_evaluator.java
@@ -2,23 +2,39 @@ package surajbab.evaluator;
 
 public class Exp_evaluator {
     public int evaluate(String Exp){
+        Exp=Exp.replaceAll(" +", "");
         int result=0;
-       Exp=Exp.replaceAll(" +", "");
-        char Operators[] ={'+','-','*','/','^'};
-        String numbers =Exp;
-        for (char operator : Operators)
-          numbers =numbers.replace(operator,' ');
-        String Numbers[] = numbers.split(" ");
-        if(Exp.charAt(1)=='+')
-            result =Integer.parseInt(Numbers[0])+Integer.parseInt(Numbers[1]);
-        if(Exp.charAt(1)=='-')
-            result =Integer.parseInt(Numbers[0])-Integer.parseInt(Numbers[1]);
-        if(Exp.charAt(1)=='*')
-            result =Integer.parseInt(Numbers[0])*Integer.parseInt(Numbers[1]);
-        if(Exp.charAt(1)=='/')
-            result =Integer.parseInt(Numbers[0])/Integer.parseInt(Numbers[1]);
-        if(Exp.charAt(1)=='^')
-            result =Integer.parseInt(Numbers[0])^Integer.parseInt(Numbers[1]);
+        Integer Numbers[] =getNumbers(Exp);
+
+        if(Exp.charAt(1)=='+') {
+            result =Numbers[0]+Numbers[1];
+        }
+        if(Exp.charAt(1)=='-') {
+            result =Numbers[0]-Numbers[1];
+        }
+        if(Exp.charAt(1)=='*') {
+            result =Numbers[0]*Numbers[1];
+        }
+        if(Exp.charAt(1)=='/') {
+            result =Numbers[0]/Numbers[1];
+        }
+        if(Exp.charAt(1)=='^') {
+            result =Numbers[0]^Numbers[1];
+        }
         return result;
+    }
+
+    private Integer[] getNumbers(String numbers) {
+        char Operators[] ={'+','-','*','/','^'};
+        for (char operator : Operators)
+            numbers =numbers.replace(operator,' ');
+
+        String numbersInString[] = numbers.split(" ");
+        Integer[] Numbers=new Integer[numbersInString.length];
+
+        for (int i = 0; i < numbersInString.length; i++)
+            Numbers[i]=Integer.parseInt(numbersInString[i]);
+
+        return Numbers;
     }
 }
